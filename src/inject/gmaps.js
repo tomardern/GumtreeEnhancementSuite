bc4a273fc1fbb348dabca35d52b330b46dfb525e
Tom Ardern <tom.ardern@gmail.com>
2013-06-17 21:34:03 +0100

    Better marker placement and performance changes

--- src/inject/gmaps.js
+++ src/inject/gmaps.js
@@ -4,9 +4,7 @@
 Variables Required
 ----------------------------*/
 ges = {}; //Gumtree Enhancement Suite
-ges.adverts = []; //Known and unknown locations
 ges.advertsString = "|"; //String of all the adverts we have seen
-ges.counter = 0;
 ges.map = {};
 ges.pageTitle = ""; //Default Page Title
 
@@ -45,12 +43,18 @@ ges.getLocations = function($listings){
 
 
 //Now that a location has been found,
-ges.onLocationFound = function(locationObj){
+ges.addToGoogleMap = function(locationObj,aniamtion){
+
+	ganimation = "";
+	if (aniamtion){
+		ganimation = google.maps.Animation.DROP;
+	}
 
 	//Now create a marker
 	var marker = new google.maps.Marker({
         map: ges.map,
         position: locationObj.latLng,
+        animation: ganimation,
         title: locationObj.location,
         link: locationObj.link
     });
@@ -63,18 +67,17 @@ ges.onLocationFound = function(locationObj){
 
 
 
-ges.updateLongLat = function(index){
+ges.addLocationsToMap = function(locations,animation){
 
 
 	//If there is an advert with this index
-	if (typeof ges.adverts[index] != "undefined"){
-		advert = ges.adverts[index];
+	if (locations.length){
+		advert = locations.shift(); //Get the first from the array
 
 		$element = $("<div />");
 
   		// Scrape the Page for the location of the map
   		$element.load(advert.link + " .open_map", function(){
-  			//console.log("GOT " + advert.location);
 
   			//We have injected the <a> link into the <div> - Now we have to find it again :S
 	  		var $mapImg = $(this).find("a");
@@ -91,32 +94,30 @@ ges.updateLongLat = function(index){
 	  			//Coverts the link to an appropiate long/lat array
 	  			latLng = centerMatch[1].split("%2C",2);
 
-
 	  			//Create Lat/Long Object
 	  			gLatLng = new google.maps.LatLng(parseFloat(latLng[0]), parseFloat(latLng[1]));
 
 	  			//Now we need to update the array to say we have found one
-	  			ges.adverts[index].latLng = gLatLng;
-	  			console.log(index,"Location Found",latLng);
+	  			advert.latLng = gLatLng;
+	  			//console.log("Location Found",latLng);
 
 	  			//Now that it's been found - run the call back
-
-	  			ges.onLocationFound(ges.adverts[index]);
+	  			ges.addToGoogleMap(advert,animation);
 
 
 	  		}	else {
 	  			//TODO: Google GeoCode to find the required place
-	  			console.log(index,"No Map",advert.link);
+	  			//console.log("No Map",advert.link);
 	  		}
 
 
   			//Now get the next one
-  			ges.updateLongLat(index+1);
+  			ges.addLocationsToMap(locations); //Locations has been updated
+
   		});
 
 		} else {
 			console.log("...........DONE..................");
-			console.log(ges.adverts);
 		}
 };
 
@@ -124,47 +125,46 @@ ges.updateLongLat = function(index){
 Alert on new item
 -----------------------------------------------------*/
 ges.alertOnNew = function(){
+	console.log("GES","alert","New Items");
+
+
 
 	//Set interval on checks
 	ges.interval = setInterval(function(){
 
+
 		$element = $("<div />"); //Element trick again
 		//Do a AJAX Load of this page
 		$element.load(window.location.href + "  .ad-listings li",function(){
-			counter = 0;
-			//Scan through and check if it exists
-			$("li",$element).each(function(){
-
-				id = $("a",$(this)).attr("id");
-
-				if (ges.advertsString.indexOf("|" + id + "|") == -1) {
-					//Ladies, this is a new advert we haven't seen before! Oh my.
-					counter++;
-
-					//TODO: Cache then add to DOM
-					$(".ad-listings").first().prepend($(this).addClass("ges-new").addClass("ges-added"));
-
-					//Add to array to stop adding again
-					ges.advertsString +=  id + "|";
 
 
-				}
+			//Add class
+			adverts = ges.getLocations($(this));
+
+			if (adverts.locations.length) {
+				//Insert into the main DOM
+				$(".ad-listings").before(adverts.$html);
+
+				//Store into the strings
+				ges.advertsString += adverts.asString;
+
+				console.log("GES | ADDED " + adverts.locations.length + " MORE RESULTS");
+
+				//Update the locations with Longitude/Latitude
+				ges.addLocationsToMap(adverts.locations,true);
+			}
+
 		});
 
-		console.log("GES | ADDED " + counter + " MORE RESULTS");
 
-		$gesNew = $(".ges-new");
+
+		/* $gesNew = $(".ges-new");
 
 		$gesNew.hover(function(){
 			$(this).removeClass("ges-new");
 		});
 
-	 	document.title = "(" + $gesNew.length + ") " + ges.pageTitle;
-
-
-
-	});
-
+	 	document.title = "(" + $gesNew.length + ") " + ges.pageTitle; */
 
 	}, 10000);
 
@@ -198,12 +198,10 @@ ges.loadInNextPage = function(){
 
 		//Go though and find the ones which are not here
 
-		adverts = ges.getLocations($(".ad-listings li",$element));
+		adverts = ges.getLocations($(".ad-listings li",$(this)));
 
-		//Do some tiding
-		//1. Replace main Pagination with new one
-		$("#pagination").replaceWith($("#pagination",$element).remove());
-
+		// Replace main Pagination with new one
+		$("#pagination").replaceWith($("#pagination",$(this)));
 
 		//Insert into the main DOM
 		$afterThis.after(adverts.$html);
@@ -211,10 +209,8 @@ ges.loadInNextPage = function(){
 		//Store into the strings
 		ges.advertsString += adverts.asString;
 
-
-		//Remove Adsense - I'm Sorry, it just looks neater! (Sneaky hack to remove all instances)
-		$("div[id='js_adsense_footer']").remove();
-
+		//Update the locations with Longitude/Latitude
+		ges.addLocationsToMap(adverts.locations,false);
 
 		//Update the load Trigger
 		ges.loadTrigger = $("#pagination").position().top - 1000;
@@ -276,21 +272,19 @@ initialize = function() {
 	//console.log(adverts);
 
 
-	ges.adverts = adverts.locations;
 	ges.advertsString += adverts.asString;
-
 
 	ges.pageTitle = document.title;
 
 	//Update the locations with Longitude/Latitude
-	//ges.updateLongLat(0);
+	ges.addLocationsToMap(adverts.locations,false);
 
 
 	//Load in the infinate scroll functionality
 	ges.initInfinateScroll();
 
 	//Alert on new ones
-	//ges.alertOnNew();
+	ges.alertOnNew();
 
 
 };
@@ -303,7 +297,7 @@ function loadScript() {
 	$("#main-content").css("padding-right","0px");
 
 	//Add some CSS
-	document.write("<style>.main-content-wrapper #search-results .ges-new {background:#DDFFBE;}#search-results .ges-added {background:#F5F5F5;}</style>");
+	document.write("<style>.main-content-wrapper #search-results .ges-new {background:#DDFFBE;}#search-results .ges-added {background:#F5F5F5;} .ges-page {border-top: 2px dotted #000; }</style>");
 
 	$("#search-results").prepend("<div id='ges' style='height:400px;'></div>");
 	var script = document.createElement("script");
